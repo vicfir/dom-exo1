54c0fb5f25482399da31854e0ad4f6984281fb68
vicfir <vicfir@hotmail.fr>
2023-01-12 16:00:28 +0100

    fin

---
+++ main.js
@@ -0,0 +1,18 @@
+let titre1 = document.getElementsByTagName("h1");
+console.log(titre1);
+
+let sousTitres = document.getElementsByTagName("h3");
+console.log(sousTitres);
+
+let sousTitre1 = sousTitres[0];
+let sousTitre2 = sousTitres[1];
+let sousTitre3 = sousTitres[2];
+console.log(sousTitre1,sousTitre2,sousTitre3);
+
+let para = document.getElementsByTagName("p");
+let secPara = para[1];
+console.log(secPara);
+
+let liste = document.getElementsByTagName("li");
+let li3 = liste[2];
+console.log(li3);
